623e6e284668b36042f8d404c3c9548142873ba0
daxamatox <daxamatox@gmail.com>
2025-03-28 16:52:17 +0100

    Initial quotes program

---
+++ quotes.h
@@ -0,0 +1,17 @@
+//
+// Created by rodri on 3/28/2025.
+//
+
+#ifndef QUOTES_H
+#define QUOTES_H
+
+#include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+ #define MAX_QUOTES 10
+
+ void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif //QUOTES_H
